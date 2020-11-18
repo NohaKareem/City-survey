5d6a30c8cbdb0310007aa218cd1c0b2b4f4c4000
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-11-18 00:31:59 -0500

    Added question 5.2, updated js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,11 @@
 (_ => {
     let q_input = document.querySelectorAll('.question');
     let q_text = document.querySelectorAll('.response');
-    let london_loc = document.querySelectorAll('input[name="london_loc"');
-    console.log(london_loc)
 
-    // let preLockDown = document.querySelectorAll('.preLockDown');
-
-    // update scale % value
+    // update scale value, with % unit if needed
     q_input.forEach((q, i) => {
         q.addEventListener('change', _ => {
-            q_text[i].innerHTML = q.value + "%";
+            q_text[i].innerHTML = q.value + (q_text[i].classList.contains('notPercent') ? "" : "%");
         });
     });
-
-    // offer hover tooltip
-    // preLockDown.forEach(_ => {
-    //     console.log(preLockDown)
-    // preLockDown.innerHTML = `<div class="preLockDown">pre-lockdown <span class="tooltip">druing 4 months to the COVID lockdown (around mid-March, 2020)</span></div>`
-    //     // preLockDown.addEventListener('hover', _ => {
-
-    //     // });
-    // });
-
 })();
